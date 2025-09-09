3a9389bdaec16b30c886f9fbedff8162dea240c8
Brian Ray <bray_stenovations@yahoo.com>
2025-09-09 10:19:06 -0400

    changes #3

--- 2025 Fall ISYS325 TestApp1/MainForm.Designer.cs
+++ 2025 Fall ISYS325 TestApp1/MainForm.Designer.cs
@@ -31,6 +31,10 @@
             btnPushMe = new Button();
             lblHelloWorld = new Label();
             btnClose = new Button();
+            picChangedCar = new PictureBox();
+            txtTest = new TextBox();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)picChangedCar).BeginInit();
             SuspendLayout();
             // 
             // btnPushMe
@@ -45,13 +49,14 @@
             // 
             // lblHelloWorld
             // 
-            lblHelloWorld.AutoSize = true;
+            lblHelloWorld.BorderStyle = BorderStyle.FixedSingle;
             lblHelloWorld.Font = new Font("Showcard Gothic", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblHelloWorld.Location = new Point(174, 70);
+            lblHelloWorld.Location = new Point(36, 21);
             lblHelloWorld.Name = "lblHelloWorld";
-            lblHelloWorld.Size = new Size(116, 37);
+            lblHelloWorld.Size = new Size(370, 69);
             lblHelloWorld.TabIndex = 1;
-            lblHelloWorld.Text = "label1";
+            lblHelloWorld.Text = "this is a";
+            lblHelloWorld.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // btnClose
             // 
@@ -63,17 +68,49 @@
             btnClose.UseVisualStyleBackColor = true;
             btnClose.Click += btnClose_Click;
             // 
+            // picChangedCar
+            // 
+            picChangedCar.ImageLocation = "C:\\Users\\bray_\\Downloads\\Bluecar.png";
+            picChangedCar.Location = new Point(32, 130);
+            picChangedCar.Name = "picChangedCar";
+            picChangedCar.Size = new Size(330, 119);
+            picChangedCar.TabIndex = 3;
+            picChangedCar.TabStop = false;
+            picChangedCar.Click += picBlueCar_Click;
+            // 
+            // txtTest
+            // 
+            txtTest.Font = new Font("Showcard Gothic", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtTest.Location = new Point(726, 83);
+            txtTest.Multiline = true;
+            txtTest.Name = "txtTest";
+            txtTest.Size = new Size(316, 76);
+            txtTest.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(528, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(36, 20);
+            label1.TabIndex = 4;
+            label1.Text = "&Age";
+            // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1095, 450);
+            Controls.Add(label1);
+            Controls.Add(txtTest);
+            Controls.Add(picChangedCar);
             Controls.Add(btnClose);
             Controls.Add(lblHelloWorld);
             Controls.Add(btnPushMe);
             Name = "MainForm";
             Text = "Form1";
             Load += MainForm_Load;
+            ((System.ComponentModel.ISupportInitialize)picChangedCar).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -83,5 +120,8 @@
         private Button btnPushMe;
         private Label lblHelloWorld;
         private Button btnClose;
+        private PictureBox picChangedCar;
+        private TextBox txtTest;
+        private Label label1;
     }
 }
--- 2025 Fall ISYS325 TestApp1/MainForm.cs
+++ 2025 Fall ISYS325 TestApp1/MainForm.cs
@@ -1,8 +1,11 @@
+using System.Linq.Expressions;
+
 namespace _2025_Fall_ISYS325_TestApp1
 {
     public partial class MainForm : Form
     {
         private const string HELLO_MSG = "Hello World";
+        private const string CAR_MSG = "You clicked the car!";
 
         public MainForm()
         {
@@ -12,17 +15,41 @@ namespace _2025_Fall_ISYS325_TestApp1
         private void btnPushMe_Click(object sender, EventArgs e)
         {
             lblHelloWorld.ForeColor = Color.Red;
-            lblHelloWorld.Text = HELLO_MSG;
+
+            try
+            {
+                decimal test = decimal.Parse(txtTest.Text);
+                string.Format("You are {0:C} years old", test);
+                lblHelloWorld.Text = $"You are {test:C} years old";
+            }
+            catch
+            {
+                MessageBox.Show("There was an exception!");
+            }
+            finally
+            {
+                picChangedCar.Visible = !picChangedCar.Visible;
+            }
+
         }
 
         private void MainForm_Load(object sender, EventArgs e)
         {
             lblHelloWorld.Text = "";
+
+
+        
         }
 
         private void btnClose_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void picBlueCar_Click(object sender, EventArgs e)
+        {
+            lblHelloWorld.ForeColor = Color.Black;
+            lblHelloWorld.Text = CAR_MSG;
         }
     }
 }
